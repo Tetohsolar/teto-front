9edc23fe396aa7078f4d209316a7363f56e4216d
Danilo S <45634291+danilocdesousa@users.noreply.github.com>
2023-02-22 20:28:45 -0300

    Install and configure Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap';
+import './scss/custom.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,13 @@
-import './home.css';
+import './home.scss';
 
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <button type="button" className="btn btn-primary">
+        Primary
+      </button>
+    </div>
+  );
 };
 
 export default Home;
