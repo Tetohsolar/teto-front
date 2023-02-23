d47de40d7e2df1ca858c9c106cd658138a8069db
Danilo S <45634291+danilocdesousa@users.noreply.github.com>
2023-02-22 22:05:22 -0300

    Create Sidebar Component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,14 @@
 import './home.scss';
 import Navbar from '../../components/navbar/Navbar';
+import Sidebar from '../../components/sidebar/Sidebar';
 
 const Home = () => {
   return (
     <div className="home">
-      <Navbar /> Home
+      <Navbar />
+      <div className={"d-flex wrapper"}>
+        <Sidebar />
+      </div>
     </div>
   );
 };
