cc491771158fc5513166494a97226f8581bf4c9b
Danilo S <45634291+danilocdesousa@users.noreply.github.com>
2023-02-24 22:09:31 -0300

    Update List Component

---
+++ src/components/administrator/Administrator.js
@@ -0,0 +1,52 @@
+import './administrator.scss'
+import { Link } from 'react-router-dom';
+
+const administrator = [
+  {
+    id: 1,
+    img: "https://api.dicebear.com/5.x/thumbs/svg?seed=Lucy",
+    firstName: "Anna Silva",
+    email: "anna@domain.com",
+    userType: "Admin",
+  },
+];
+
+const DataTable = (props) => {
+  return (
+    <div className="p-3 mb-3 bg-white border rounded-3">
+      <h5 className="card-content-title fw-semibold">{props.listTitle}</h5>
+      <div className="admin-table-w">
+        <div className="table-responsive">
+          <table className="table table-borderless">
+            <tbody>
+              {administrator.map((row) => {
+                return (
+                  <tr key={row.id}>
+                    <td className="td-img">
+                      <img className="header-avatar" src={row.img} alt="Avatar"
+                      />
+                    </td>
+                    <td>{row.firstName}</td>
+                    <td>{row.email}</td>
+                    <td>
+                      <span className="badge bg-light text-success">
+                        {row.userType}
+                      </span>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <Link to={"/users/new"} className="btn btn-primary text-light">
+        Editar perfil
+      </Link>
+    </div>
+  );
+};
+
+export default DataTable;
+
+
--- src/pages/list/List.js
+++ src/pages/list/List.js
@@ -1,8 +1,27 @@
+import { useContext } from 'react';
+import Navbar from '../../components/navbar/Navbar';
+import Sidebar from '../../components/sidebar/Sidebar';
+import { SidebarWrapperContext } from '../../context/SidebarWrapperContext';
+import DataTable from '../../components/datatable/DataTable';
+import Administrator from '../../components/administrator/Administrator';
+import '../pages.scss';
 import './list.scss';
 
 const List = () => {
+  const { sidebarWrapper } = useContext(SidebarWrapperContext);
+  const pageTitle= 'Perfil';
   return (
-    <div className="list">List</div>
+    <div>
+      <Navbar />
+      <div className={sidebarWrapper ? "d-flex wrapper toggled" : "d-flex wrapper"}>
+        <Sidebar activeButtonProfile='active' />
+        <div id="page-content-wrapper" className="container-fluid bg-home py-4">
+          <h5 className='pb-3'>{pageTitle}</h5>
+          <Administrator listTitle="Minha conta" />
+          <DataTable listTitle="Outros usuários" />
+        </div>
+      </div>
+    </div>
   );
 };
 
