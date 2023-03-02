85d884e7fe4a3600c6fffae438f8d04578398c94
Danilo S <45634291+danilocdesousa@users.noreply.github.com>
2023-03-02 16:12:53 -0300

    Update the customer page

--- src/components/customer-data-table/CustomerDataTable.js
+++ src/components/customer-data-table/CustomerDataTable.js
@@ -21,9 +21,9 @@ const CustomerDataTable = (props) => {
           </svg>
         </button>
       </div>
-      <div className="table-w">
+      <div>
         <div className='table-responsive'>
-        <table className="table table-borderless">
+        <table className="table">
           <tbody>
             {users.map((user) => {
               return (
@@ -33,7 +33,7 @@ const CustomerDataTable = (props) => {
                   <td>{user.email}</td>
                   <td><span className="badge bg-light text-secondary">{user.userType}</span></td>
                   <td>
-                    <div className="d-flex gap-2">
+                    <div className="d-flex gap-2 justify-content-end">
                       <button type="button" className="btn btn-light btn-sm text-primary d-flex align-items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-fill" viewBox="0 0 16 16">
                           <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
