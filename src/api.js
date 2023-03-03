6574911007138e4f729e975902b7e716691c4d89
mentrixmax <mentrixmax@gmail.com>
2023-03-02 22:43:21 -0300

    mudando a porta do cors

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: "http://ec2-3-223-6-199.compute-1.amazonaws.com:3001",
+  baseURL: "http://ec2-3-223-6-199.compute-1.amazonaws.com:3000",
 
 })
 
