245614ae7ca3d86a41564738163b378bdde8c18e
Danilo S <45634291+danilocdesousa@users.noreply.github.com>
2023-03-23 04:29:18 -0300

    Fix some components

--- src/components/BusinessDataTable/index.js
+++ src/components/BusinessDataTable/index.js
@@ -1,5 +1,6 @@
 import './style.scss'
 import NewBusiness from '../../pages/business/new';
+import { AiOutlinePartition } from 'react-icons/ai';
 
 const business = [
   {
@@ -33,6 +34,21 @@ const business = [
     status: "Em aberto",
     power: "2.3",
     amount: "49.999,99",
+  },
+];
+
+const selectOptions = [
+  {
+    value: "abertos",
+    label: "Em aberto",
+  },
+  {
+    value: "fechados",
+    label: "Fechados",
+  },
+  {
+    value: "perdidos",
+    label: "Perdidos",
   },
 ];
 
@@ -78,13 +94,11 @@ const BusinessDataTable = (props) => {
         </div>
         <form className="mb-3 justify-content-end">
           <div className="row">
-            <di className="col-md-auto">
+            <div className="col-md-auto">
               <select className="form-select" aria-label="Selecionar">
-                <option selected>Em aberto</option>
-                <option value="1">Fechados</option>
-                <option value="2">Perdidos</option>
+                {selectOptions.map((option) => (<option key={option.value} value={option.value} >{option.label}</option>))}
               </select>
-            </di>
+            </div>
           </div>
         </form>
       </div>
@@ -175,13 +189,13 @@ const BusinessDataTable = (props) => {
               <div className="btn-toolbar justify-content-end" role="toolbar" aria-label="Toolbar with button groups">
                 <div className="btn-group" role="group" aria-label="First group">
                   <button type="button" className="d-flex btn btn-outline-secondary text-primary align-items-center"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-left-short" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/></svg>
+                    <path fillRule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/></svg>
                   </button>
                   <button type="button" className="btn btn-outline-secondary text-primary active">1</button>
                   <button type="button" className="btn btn-outline-secondary text-primary">2</button>
                   <button type="button" className="btn btn-outline-secondary text-primary">3</button>
                   <button type="button" className="d-flex btn btn-outline-secondary text-primary align-items-center"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right-short" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/></svg>
+                    <path fillRule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/></svg>
                   </button>
                 </div>
               </div>
