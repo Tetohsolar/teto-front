189e550388895759c754ec11626fc011f670e2a3
RaianeDarla <109601472+RaianeDarla@users.noreply.github.com>
2023-03-27 16:33:47 -0300

    mudanças realizadas

--- src/components/SixMonthsBusinessDataTable/index.js
+++ src/components/SixMonthsBusinessDataTable/index.js
@@ -5,6 +5,7 @@ import { AuthContext } from '../../context/AuthContext';
 import { useContext, useEffect, useMemo, useState } from 'react';
 import { format } from 'date-fns';
 import Pagination from '../pagination/Pagination';
+import {BsFillSendFill, BsPencilFill } from "react-icons/bs";
 
 
 
@@ -139,24 +140,13 @@ const SixMonthsBusinessDataTable = (props) => {
                                 type="button"
                                 className="btn btn-light btn-sm text-primary d-flex align-items-center"
                               >
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-send-fill" viewBox="0 0 16 16">
-                                  <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z"/>
-                                </svg>
+                                <BsFillSendFill/>
                               </button>
                               <button
                                 type="button"
                                 className="btn btn-light btn-sm text-primary d-flex align-items-center"
                               >
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  width="16"
-                                  height="16"
-                                  fill="currentColor"
-                                  className="bi bi-pencil-fill"
-                                  viewBox="0 0 16 16"
-                                >
-                                  <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
-                                </svg>
+                                <BsPencilFill/>
                               </button>
                             </div>
                           </td>
