03ffc16178658652df25a26be41780c46b8ae366
Danilo S <45634291+danilocdesousa@users.noreply.github.com>
2023-03-28 10:45:05 -0300

    Update the single business page

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -66,7 +66,7 @@ const Sidebar = (props) => {
               data-bs-target="#exampleModal"
             > */}
             {/* Button trigger modal*/}
-            <button className={`nav-link`} data-bs-toggle="modal" data-bs-target="#exampleModa2l">
+            <button className={`nav-link ${props.activeButtonSingleBusiness}`} data-bs-toggle="modal" data-bs-target="#exampleModa2l">
               <div className='d-flex align-items-center gap-2'>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-plus-circle-fill" viewBox="0 0 16 16">
                   <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z" />
--- src/pages/business/single/index.js
+++ src/pages/business/single/index.js
@@ -1,20 +1,22 @@
+import { useContext } from "react";
 import Navbar from "../../../components/navbar/Navbar";
 import Sidebar from "../../../components/sidebar/Sidebar";
-import SingleBusinessBrief from "../../../components/SingleBusinessBrief";
+import SixMonthsCharts from "../../../components/SixMonthsCharts";
 import { SidebarWrapperContext } from "../../../context/SidebarWrapperContext";
 
 export default function SingleBusiness() {
-  const pageTitle = "Novo negócio";
+  const { sidebarWrapper } = useContext(SidebarWrapperContext);
+  const pageTitle = "Resumo";
   return (
     <div>
       <Navbar />
       <div
-        className={SidebarWrapperContext ? "d-flex wrapper toggled" : "d-flex wrapper"}
+        className={sidebarWrapper ? "d-flex wrapper toggled" : "d-flex wrapper"}
       >
-        <Sidebar activeButtonBusiness="active" />
+        <Sidebar activeButtonSingleBusiness="active" />
         <div id="page-content-wrapper" className="container-fluid bg-home py-4">
           <h5 className="pb-3">{pageTitle}</h5>
-          <SingleBusinessBrief listTitle="Resumo" />
+          <SixMonthsCharts listTitle="Resumo" />
         </div>
       </div>
     </div>
