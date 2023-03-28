b819d5b6565909f0d3b225d12a3920fdb4ac5649
Danilo S <45634291+danilocdesousa@users.noreply.github.com>
2023-03-28 11:08:06 -0300

    Update the single business page

--- src/components/SingleBusinessBrief/index.js
+++ src/components/SingleBusinessBrief/index.js
@@ -7,18 +7,34 @@ export default function SingleBusinessBrief(props) {
     <div className="p-3 mb-3 bg-white rounded-3">
       <h5 className="card-content-title fw-semibold mb-3">{props.listTitle}</h5>
       <div className="row">
-        <div className="col-sm-6 mb-3 mb-sm-0">
+        <div className="col-lg-3 col-sm-6 mb-3 mb-lg-0">
           <div className="card border-light-subtle">
             <div className="card-body">
-              <h6 className="card-title">Negócios fechados</h6>
+              <h6 className="card-title">Status</h6>
               <TinyBarGainChart />
             </div>
           </div>
         </div>
-        <div className="col-sm-6">
+        <div className="col-lg-3 col-sm-6 mb-3 mb-lg-0">
           <div className="card border-light-subtle">
             <div className="card-body">
-              <h6 className="card-title">Negócios perdidos</h6>
+              <h6 className="card-title">Contato</h6>
+              <TinyBarLossesChart />
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 col-sm-6 mb-3 mb-sm-0">
+          <div className="card border-light-subtle">
+            <div className="card-body">
+              <h6 className="card-title">Responsável</h6>
+              <TinyBarLossesChart />
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 col-sm-6">
+          <div className="card border-light-subtle">
+            <div className="card-body">
+              <h6 className="card-title">Proposta</h6>
               <TinyBarLossesChart />
             </div>
           </div>
--- src/pages/business/single/index.js
+++ src/pages/business/single/index.js
@@ -1,12 +1,13 @@
 import { useContext } from "react";
 import Navbar from "../../../components/navbar/Navbar";
 import Sidebar from "../../../components/sidebar/Sidebar";
+import SingleBusinessBrief from "../../../components/SingleBusinessBrief";
 import SixMonthsCharts from "../../../components/SixMonthsCharts";
 import { SidebarWrapperContext } from "../../../context/SidebarWrapperContext";
 
 export default function SingleBusiness() {
   const { sidebarWrapper } = useContext(SidebarWrapperContext);
-  const pageTitle = "Resumo";
+  const pageTitle = "Novo negócio";
   return (
     <div>
       <Navbar />
@@ -16,7 +17,7 @@ export default function SingleBusiness() {
         <Sidebar activeButtonSingleBusiness="active" />
         <div id="page-content-wrapper" className="container-fluid bg-home py-4">
           <h5 className="pb-3">{pageTitle}</h5>
-          <SixMonthsCharts listTitle="Resumo" />
+          <SingleBusinessBrief listTitle="Resumo" />
         </div>
       </div>
     </div>
