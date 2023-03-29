b429e3db7b9922f216d8678b463be80b9d56991a
Danilo S <45634291+danilocdesousa@users.noreply.github.com>
2023-03-29 12:32:08 -0300

    Update the table structure

--- src/components/SingleBusinessDetails/index.js
+++ src/components/SingleBusinessDetails/index.js
@@ -1,24 +1,83 @@
 import "./style.scss";
 
 export default function SingleBusinessDetails(props) {
+  const basicInfoData = {
+    criadoModificadoEm: "13/03/2023",
+    nome: "João dos Santos",
+    tipoDePessoa: "PF",
+    cpfCnpj: "123.456.789-00",
+    telefone: "(88) 91234-5678",
+    whatsapp: "(88) 91234-5678",
+    email: "meu@email.com",
+    informacoesAdicionais: "Negócio residencial.",
+  };
+
+  const addressData = {
+    rua: "Rua ABC",
+    bairro: "Centro",
+    cep: "12300-000",
+    estado: "Ceará",
+    cidade: "Sobral",
+  };
+
   return (
     <div className="p-3 mb-3 bg-white rounded-3">
       <h5 className="card-content-title mb-3 fw-semibold">{props.title}</h5>
       <div className="row">
-        <div className="col-sm-6 mb-3 mb-sm-0">
+        <div className="mb-3 col-lg-6">
           <div className="card border-light-subtle">
             <div className="card-body">
-              <h6 className="card-title">Informações básicas</h6>
+              <h6 className="card-title mb-3">Informações da geradora</h6>
               <div className="table-responsive">
-                <table className="table caption-top table-sm">
+                <table className="table caption-top table-sm mb-0">
                   <tbody>
                     <tr>
-                      <td>a</td>
-                      <td>b</td>
+                      <th scope="row" className="fw-semibold">
+                        Cliente
+                      </th>
+                      <td>{basicInfoData.criadoModificadoEm}</td>
                     </tr>
                     <tr>
-                      <td>c</td>
-                      <td>d</td>
+                      <th scope="row" className="fw-semibold">
+                        Usuário
+                      </th>
+                      <td>{basicInfoData.nome}</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="fw-semibold">
+                        Fator solar
+                      </th>
+                      <td>{basicInfoData.tipoDePessoa}</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="fw-semibold">
+                        Tipo de telhado
+                      </th>
+                      <td>{basicInfoData.cpfCnpj}</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="fw-semibold">
+                        Rateios
+                      </th>
+                      <td>{basicInfoData.telefone}</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="fw-semibold">
+                        Modalidade
+                      </th>
+                      <td>{basicInfoData.whatsapp}</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="fw-semibold">
+                        Grupo
+                      </th>
+                      <td>{basicInfoData.email}</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="fw-semibold">
+                        Subgrupo
+                      </th>
+                      <td>{basicInfoData.informacoesAdicionais}</td>
                     </tr>
                   </tbody>
                 </table>
@@ -26,20 +85,42 @@ export default function SingleBusinessDetails(props) {
             </div>
           </div>
         </div>
-        <div className="col-sm-6 mb-3 mb-sm-0">
+        <div className="mb-3 col-lg-6">
           <div className="card border-light-subtle">
             <div className="card-body">
-              <h6 className="card-title">Endereço</h6>
+              <h6 className="card-title mb-3">Informações da geradora</h6>
               <div className="table-responsive">
-                <table className="table caption-top table-sm">
+                <table className="table caption-top table-sm mb-0">
                   <tbody>
                     <tr>
-                      <td>a</td>
-                      <td>b</td>
+                      <th scope="row" className="fw-semibold">
+                        Rua
+                      </th>
+                      <td>{addressData.rua}</td>
                     </tr>
                     <tr>
-                      <td>c</td>
-                      <td>d</td>
+                      <th scope="row" className="fw-semibold">
+                        Bairro
+                      </th>
+                      <td>{addressData.bairro}</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="fw-semibold">
+                        CEP
+                      </th>
+                      <td>{addressData.cep}</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="fw-semibold">
+                        Estado
+                      </th>
+                      <td>{addressData.estado}</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="fw-semibold">
+                      Cidade
+                      </th>
+                      <td>{addressData.cidade}</td>
                     </tr>
                   </tbody>
                 </table>
