9d0dd07592c4fc947bbfac96ed283a3ee9dcacc6
Danilo S <45634291+danilocdesousa@users.noreply.github.com>
2023-03-29 13:10:04 -0300

    Update the table structure

--- src/components/SingleBusinessDataTable/index.js
+++ src/components/SingleBusinessDataTable/index.js
@@ -1,5 +1,13 @@
 import "./style.scss";
 import NewBusiness from "../../pages/business/new";
+import {
+  BsFillPieChartFill,
+  BsFillSendFill,
+  BsPencilFill,
+  BsFillTrash3Fill,
+} from "react-icons/bs";
+import { AiFillPlusSquare } from "react-icons/ai";
+import { useState } from "react";
 
 const business = [
   {
@@ -37,16 +45,62 @@ const business = [
 ];
 
 export default function SingleBusinessDataTable(props) {
-  const totalValue = "99";
+  const [situation, setSituation] = useState([]);
+  const selectOptions = [
+    {
+      value: "Aberta",
+      label: "Em aberto",
+    },
+    {
+      value: "Ganhos",
+      label: "Fechados",
+    },
+    {
+      value: "Perdas",
+      label: "Perdidos",
+    },
+  ];
+
   return (
     <div className="p-3 mb-3 bg-white rounded-3">
-      <h5 className="card-content-title mb-3 fw-semibold">{props.title}</h5>
+      <div className="d-flex flex-column flex-sm-row justify-content-between">
+        <h5 className="card-content-title fw-semibold mb-3">{props.title}</h5>
+        <button
+          className="btn btn-primary text-light d-flex align-items-center justify-content-sm-start justify-content-center gap-2"
+          type="submit"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+        >
+          <AiFillPlusSquare />
+          Novo negócio
+        </button>
+      </div>
       <NewBusiness />
+      <hr className="my-3 text-body-tertiary" />
+      <div className="d-flex flex-column flex-md-row justify-content-end">
+        <form className="mb-3 justify-content-end">
+          <div className="row">
+            <div className="col-md-auto">
+              <select
+                className="form-select"
+                aria-label="Selecionar"
+                onChange={(e) => setSituation(e.target.value)}
+              >
+                {selectOptions.map((option) => (
+                  <option key={option.value} value={option.value}>
+                    {option.label}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+        </form>
+      </div>
       <div className="row">
         <div className="mb-3 mb-sm-0">
           <div className="card border-light-subtle">
             <div className="card-body">
-              <h6 className="card-title">{`Total de negócios: ${totalValue}`}</h6>
+              <h6 className="card-title">Informações da geradora</h6>
               <div className="table-responsive">
                 <table className="table caption-top table-sm">
                   <thead>
@@ -80,31 +134,25 @@ export default function SingleBusinessDataTable(props) {
                                 type="button"
                                 className="btn btn-light btn-sm text-primary d-flex align-items-center"
                               >
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  width="16"
-                                  height="16"
-                                  fill="currentColor"
-                                  className="bi bi-send-fill"
-                                  viewBox="0 0 16 16"
-                                >
-                                  <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z" />
-                                </svg>
+                                <BsFillPieChartFill />
                               </button>
                               <button
                                 type="button"
                                 className="btn btn-light btn-sm text-primary d-flex align-items-center"
                               >
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  width="16"
-                                  height="16"
-                                  fill="currentColor"
-                                  className="bi bi-pencil-fill"
-                                  viewBox="0 0 16 16"
-                                >
-                                  <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
-                                </svg>
+                                <BsFillSendFill />
+                              </button>
+                              <button
+                                type="button"
+                                className="btn btn-light btn-sm text-primary d-flex align-items-center"
+                              >
+                                <BsPencilFill />
+                              </button>
+                              <button
+                                type="button"
+                                className="btn btn-light btn-sm text-danger d-flex align-items-center"
+                              >
+                                <BsFillTrash3Fill />
                               </button>
                             </div>
                           </td>
