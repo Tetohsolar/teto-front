e1e7265a48e38a8cd1d3615b4504803e717eefdf
Danilo S <45634291+danilocdesousa@users.noreply.github.com>
2023-03-29 13:20:29 -0300

    Update the table structure

--- src/components/SingleBusinessSizing/index.js
+++ src/components/SingleBusinessSizing/index.js
@@ -41,7 +41,19 @@ export default function SingleBusinessSizing(props) {
         <div className="mb-3 col-lg-6">
           <div className="card border-light-subtle">
             <div className="card-body">
-              <h6 className="card-title mb-3">Informações da geradora</h6>
+              <div className="d-flex flex-column flex-sm-row justify-content-between">
+                <h6 className="card-title mb-3">Informações da geradora</h6>
+                <button
+                  className="btn btn-light text-primary d-flex align-items-center justify-content-sm-start justify-content-center gap-2"
+                  type="submit"
+                  data-bs-toggle="modal"
+                  data-bs-target="#exampleModal"
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                    <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+                  </svg>
+                </button>
+              </div>
               <div className="table-responsive">
                 <table className="table caption-top table-sm mb-0">
                   <tbody>
@@ -128,7 +140,19 @@ export default function SingleBusinessSizing(props) {
             <div className="mb-3">
               <div className="card border-light-subtle">
                 <div className="card-body">
-                  <h6 className="card-title mb-3">Informações do sistema</h6>
+                  <div className="d-flex flex-column flex-sm-row justify-content-between">
+                    <h6 className="card-title mb-3">Informações da geradora</h6>
+                    <button
+                      className="btn btn-light text-primary d-flex align-items-center justify-content-sm-start justify-content-center gap-2"
+                      type="submit"
+                      data-bs-toggle="modal"
+                      data-bs-target="#exampleModal"
+                    >
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                        <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+                      </svg>
+                    </button>
+                  </div>
                   <div className="table-responsive">
                     <table className="table caption-top table-sm mb-0">
                       <tbody>
@@ -171,7 +195,19 @@ export default function SingleBusinessSizing(props) {
             <div className="mb-3 mb-sm-0">
               <div className="card border-light-subtle">
                 <div className="card-body">
-                  <h6 className="card-title mb-3">Descrição do sistema</h6>
+                  <div className="d-flex flex-column flex-sm-row justify-content-between">
+                    <h6 className="card-title mb-3">Descrição do sistema</h6>
+                    <button
+                      className="btn btn-light text-primary d-flex align-items-center justify-content-sm-start justify-content-center gap-2"
+                      type="submit"
+                      data-bs-toggle="modal"
+                      data-bs-target="#exampleModal"
+                    >
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                        <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+                      </svg>
+                    </button>
+                  </div>
                   <div className="table-responsive">
                     <table className="table caption-top table-sm mb-0">
                       <tbody>
