05abda84f85b1406b2b9977177decb4c58c3e858
mentrixmax <mentrixmax@gmail.com>
2023-04-20 18:53:23 -0300

    Componentisando o rateio.

--- src/components/business-form/index.js
+++ src/components/business-form/index.js
@@ -18,6 +18,7 @@ import { BsFillTrash3Fill, BsPencilFill } from 'react-icons/bs';
 import MyModal from '../communs/ModalDelete';
 import { AiFillPlusSquare, AiOutlinePlus } from 'react-icons/ai';
 import TabelaProdutoEditavel from '../prods';
+import TabelaRateioBusiness from '../rateio-table';
 
 function PhoneInput(props) {
   return (
@@ -1404,153 +1405,10 @@ const BusinessForm = (props) => {
                   <div className="mb-3 mb-sm-0">
                     <div className="card border-light-subtle">
                       <div className="card-body">
-                        <div className="table-responsive">
-                          <table className="table caption-top table-sm">
-                            <thead>
-                              <tr>
-                                <th scope="col" className='tamanhoM'>Modalidade</th>
-                                <th scope="col">Grupo</th>
-                                <th scope="col">SubGrupo</th>
-                                <th scope="col" className='alinhaCenter' >Consumo</th>
-                                <th scope="col" className='alinhaCenter'>Dem. FP.</th>
-                                <th scope="col" className='alinhaCenter'>Ener. F. P. </th>
-                                <th scope="col" className='alinhaCenter'>Dem. P</th>
-                                <th scope="col" className='alinhaCenter'>Eng. P</th>
-                                <th scope="col" className='alinhaCenter'>G.Sugerida</th>
-                                <th scope="col" className='alinhaCenter'>C.I.P</th>
-                                <th scope="col"></th>
-                              </tr>
-                            </thead>
-
-                            <tbody>
-                              {dados.map((item) => {
-                                return (
-                                  <tr key={item.id}>
-                                    <td>
-                                      <select className='form-select tamanhoModalidade' value={item.modality} onChange={e => { handleEdit(item.id, 'modality', e.target.value); }}>
-                                        <option value="Convencional">Convencional</option>
-                                        <option value="HA">Horos. Azul</option>
-                                        <option value="HV">Horos. Verde</option>
-                                        <option value="Rural">Rural</option>
-                                      </select>
-                                    </td>
-                                    <td>
-                                      <select className="form-select tamanhoTabela" id="inputGrupo" value={item.group} onChange={(e) => { handleEdit(item.id, 'group', e.target.value) }} >
-                                        <option value="A">A </option>
-                                        <option value="B">B </option>
-                                      </select>
-                                    </td>
-                                    <td>
-                                      <select className="form-select" id="inputGrupo" value={item.subgroup} onChange={(e) => { handleEdit(item.id, 'subgroup', e.target.value) }} >
-                                        <option value="">Selecione</option>
-                                        <option value="A3">A3</option>
-                                        <option value="A4">A4</option>
-                                        <option value="B1">B1</option>
-                                        <option value="B2">B2</option>
-                                      </select>
-                                    </td>
-                                    <td>
-                                      <input
-                                        type="text" className='form-control tamanhoTabela alinhaDireita '
-                                        value={item.avgconsumption}
-                                        onChange={e => handleEdit(item.id, 'avgconsumption', e.target.value)}
-                                        onBlur={() => {
-                                          calculaGeracaoTotal()
-                                        }}
-
-                                      />
-                                    </td>
-                                    <td>
-
-                                      <input
-                                        type="number" className='form-control tamanhoTabela alinhaDireita '
-                                        value={item.demandaFP}
-                                        onChange={e => handleEdit(item.id, 'demandaFP', e.target.value)}
-                                        onBlur={() => {
-                                          calculaGeracaoTotal()
-                                        }}
-
-                                      />
-
-                                    </td>
-                                    <td className='alinhaDireita'>
-                                      <input
-                                        type="text" pattern="[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?" className='form-control tamanhoTabela alinhaDireita '
-                                        value={item.energiaFP}
-                                        onChange={e => handleEdit(item.id, 'energiaFP', e.target.value)}
-                                        onBlur={() => {
-                                          calculaGeracaoTotal()
-                                        }}
-
-                                      />
-                                    </td>
-                                    <td className='alinhaDireita'>
-                                      <input
-                                        type="text" className='form-control tamanhoTabela alinhaDireita '
-                                        value={item.demandaP}
-                                        onChange={e => handleEdit(item.id, 'demandaP', e.target.value)}
-                                        onBlur={() => {
-                                          calculaGeracaoTotal()
-                                        }}
-
-                                      />
-                                    </td>
-                                    <td className='alinhaDireita'>
-
-                                      <input
-                                        type="text" className='form-control tamanhoTabela alinhaDireita '
-                                        value={item.energiaP}
-                                        onChange={e => handleEdit(item.id, 'energiaP', e.target.value)}
-                                        onBlur={() => {
-                                          calculaGeracaoTotal()
-                                        }}
-
-                                      />
-                                    </td>
-                                    <td className='alinhaDireita'>
-                                      <input
-                                        type="text" className='form-control tamanhoTabela alinhaDireita ' readOnly
-                                        value={item.suggestedGeneration}
-                                        onChange={e => handleEdit(item.id, 'suggestedGeneration', e.target.value)}
-
-                                      />
-                                    </td>
-                                    <td className='alinhaDireita'>
-                                      <input
-                                        type="text" className='form-control tamanhoTabela alinhaDireita '
-                                        value={item.CIP}
-                                        onChange={e => handleEdit(item.id, 'CIP', e.target.value)}
-                                      />
-                                    </td>
-                                    <td>
-
-                                      <div className="d-flex gap-2 justify-content-end">
-                                        <button
-                                          type="button"
-                                          className="btn btn-light btn-sm text-primary d-flex align-items-center" onClick={handleAdd}
-                                        >
-                                          <AiOutlinePlus />
-                                        </button>
-
-
-                                        <button
-                                          type="button"
-                                          className="btn btn-light btn-sm text-danger d-flex align-items-center"
-                                          data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={() => {
-                                            setIdSelected(item.id)
-                                          }}
-                                        >
-                                          <BsFillTrash3Fill />
-                                          <MyModal userId={item.id} uc=" o rateio" onClick={handleAfterDel} />
-                                        </button>
-                                      </div>
-                                    </td>
-                                  </tr>
-                                );
-                              })}
-                            </tbody>
-                          </table>
-                        </div>
+                      <TabelaRateioBusiness token={token} dados={dados} handleEdit={handleEdit} 
+                   handleAdd={handleAdd} setIdSelected={setIdSelected}
+                   handleAfterDel={handleAfterDel}  calculaGeracaoTotal={calculaGeracaoTotal}
+                   />
                       </div>
                     </div>
                   </div>
