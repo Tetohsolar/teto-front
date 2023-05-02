8018a9e36d72ea84888693cced996d19df4d2b2d
Danilo S <45634291+danilocdesousa@users.noreply.github.com>
2023-05-02 10:23:31 -0300

    Update SystemTypeForm component

--- src/components/business-form/SystemTypeForm.js
+++ src/components/business-form/SystemTypeForm.js
@@ -1,3 +1,255 @@
+import * as React from "react";
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Paper,
+  Select,
+  Typography,
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+
 export default function SystemTypeForm() {
-  return <h2>Tipo de sistema</h2>;
+  const [tipo, setTipo] = React.useState("");
+
+  const handleChange = (event) => {
+    setTipo(event.target.value);
+  };
+
+  const personType = ["Física", "Jurídica"];
+
+  return (
+    <React.Fragment>
+      <box>
+        <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
+          Rateios
+        </Typography>
+        <Paper variant="outlined" sx={{ p: { xs: 2, md: 3 }}}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={4}>
+              <FormControl fullWidth size="small">
+                <InputLabel>Estado</InputLabel>
+                <Select
+                  id="demo-simple-select-type"
+                  value={tipo}
+                  label="Estado"
+                  onChange={handleChange}
+                >
+                  {personType.map((item) => (
+                    <MenuItem value={item}>{item}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <FormControl fullWidth size="small">
+                <InputLabel>Estado</InputLabel>
+                <Select
+                  id="demo-simple-select-type"
+                  value={tipo}
+                  label="Estado"
+                  onChange={handleChange}
+                >
+                  {personType.map((item) => (
+                    <MenuItem value={item}>{item}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <FormControl fullWidth size="small">
+                <InputLabel>Estado</InputLabel>
+                <Select
+                  id="demo-simple-select-type"
+                  value={tipo}
+                  label="Estado"
+                  onChange={handleChange}
+                >
+                  {personType.map((item) => (
+                    <MenuItem value={item}>{item}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={12} sm={3}>
+              <FormControl fullWidth size="small">
+                <InputLabel>Estado</InputLabel>
+                <Select
+                  id="demo-simple-select-type"
+                  value={tipo}
+                  label="Estado"
+                  onChange={handleChange}
+                >
+                  {personType.map((item) => (
+                    <MenuItem value={item}>{item}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={3}>
+              <FormControl fullWidth size="small">
+                <InputLabel>Estado</InputLabel>
+                <Select
+                  id="demo-simple-select-type"
+                  value={tipo}
+                  label="Estado"
+                  onChange={handleChange}
+                >
+                  {personType.map((item) => (
+                    <MenuItem value={item}>{item}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={3}>
+              <FormControl fullWidth size="small">
+                <InputLabel>Estado</InputLabel>
+                <Select
+                  id="demo-simple-select-type"
+                  value={tipo}
+                  label="Estado"
+                  onChange={handleChange}
+                >
+                  {personType.map((item) => (
+                    <MenuItem value={item}>{item}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={3}>
+              <TextField
+                required
+                id="phone"
+                name="phone"
+                label="Telefone"
+                fullWidth
+                size="small"
+              />
+            </Grid>
+          </Grid>
+        </Paper>
+
+        <Paper variant="outlined" sx={{ p: { xs: 2, md: 3 }, mt: 2 }}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={4}>
+              <FormControl fullWidth size="small">
+                <InputLabel>Estado</InputLabel>
+                <Select
+                  id="demo-simple-select-type"
+                  value={tipo}
+                  label="Estado"
+                  onChange={handleChange}
+                >
+                  {personType.map((item) => (
+                    <MenuItem value={item}>{item}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <FormControl fullWidth size="small">
+                <InputLabel>Estado</InputLabel>
+                <Select
+                  id="demo-simple-select-type"
+                  value={tipo}
+                  label="Estado"
+                  onChange={handleChange}
+                >
+                  {personType.map((item) => (
+                    <MenuItem value={item}>{item}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <FormControl fullWidth size="small">
+                <InputLabel>Estado</InputLabel>
+                <Select
+                  id="demo-simple-select-type"
+                  value={tipo}
+                  label="Estado"
+                  onChange={handleChange}
+                >
+                  {personType.map((item) => (
+                    <MenuItem value={item}>{item}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={12} sm={3}>
+              <FormControl fullWidth size="small">
+                <InputLabel>Estado</InputLabel>
+                <Select
+                  id="demo-simple-select-type"
+                  value={tipo}
+                  label="Estado"
+                  onChange={handleChange}
+                >
+                  {personType.map((item) => (
+                    <MenuItem value={item}>{item}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={3}>
+              <FormControl fullWidth size="small">
+                <InputLabel>Estado</InputLabel>
+                <Select
+                  id="demo-simple-select-type"
+                  value={tipo}
+                  label="Estado"
+                  onChange={handleChange}
+                >
+                  {personType.map((item) => (
+                    <MenuItem value={item}>{item}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={3}>
+              <FormControl fullWidth size="small">
+                <InputLabel>Estado</InputLabel>
+                <Select
+                  id="demo-simple-select-type"
+                  value={tipo}
+                  label="Estado"
+                  onChange={handleChange}
+                >
+                  {personType.map((item) => (
+                    <MenuItem value={item}>{item}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={3}>
+              <TextField
+                required
+                id="phone"
+                name="phone"
+                label="Telefone"
+                fullWidth
+                size="small"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            variant="outlined"
+            startIcon={<DeleteIcon />}
+            size="small"
+            sx={{ mt: 2 }}
+          >
+            Excluir
+          </Button>
+        </Paper>
+        <Button variant="outlined" size="small" sx={{ mt: 2 }}>
+          Novo rateio
+        </Button>
+      </box>
+    </React.Fragment>
+  );
 }
