67bccd99d894977654ec86a6427a03cd61c178b8
Alexandre Rocha <alexandresobral2004@gmail.com>
2023-05-04 10:23:30 -0300

    update_biblioteca_password

--- src/components/updatePasswordUser/index.js
+++ src/components/updatePasswordUser/index.js
@@ -5,12 +5,12 @@ import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useNavigate } from 'react-router-dom';
 import api from '../../api';
-
+import { FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
 
 
 const UpdatePasswordUser = (props) => {
-  const { token, idLogged} = useContext(AuthContext)
-  
+  const { token, idLogged } = useContext(AuthContext)
+
   async function findUserById() {
 
     await api.get(`/user/get/${idLogged}`, {
@@ -18,17 +18,17 @@ const UpdatePasswordUser = (props) => {
         'Authorization': `Basic ${token}`
       }
     })
-      .then((response) => { 
-        setEmail (response.data.email)
-        
+      .then((response) => {
+        setEmail(response.data.email)
+
       })
   }
   useEffect(() => {
-   findUserById ()
+    findUserById()
 
     return () => { }
   })
-  
+
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [password2, setPassword2] = useState('')
@@ -98,28 +98,41 @@ const UpdatePasswordUser = (props) => {
       <form  >
         <div className="row g-3">
           <div className="col-md-5">
-            <label htmlFor="inputEmail" className="form-label">
-              Email:
-            </label>
-            <input type="email" readOnly  className="form-control" id="inputEmail" value={email} onChange={(e) => setEmail(e.target.value)} />
+
+
+            <TextField id="inputFornecedor" maxLength={50} className="form-control"
+              inputProps={{ readonly: true }}
+              label="E-mail(somente leitura)"
+              variant="outlined"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)} />
           </div>
         </div>
-
+        <br />
         <div className="row-md-5">
-          <div className="col-4">
-            <label htmlFor="inputPhoneNumber" className="form-label">
-              Nova Senha:
-            </label>
-            <input type="password" className="form-control" id="inputSenha" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <div className="col-3">
+            <TextField id="inputPassword"
+              maxLength={12}
+              className="form-control"
+              type='password'
+              label="Senha"
+              variant="outlined"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)} />
 
           </div>
         </div>
+        <br />
         <div className="row-md-5">
-          <div className="col-4">
-            <label htmlFor="inputPhoneNumber" className="form-label">
-              Confirme a Senha:
-            </label>
-            <input type="password" className="form-control" id="inputSenha" value={password2} onChange={(e) => setPassword2(e.target.value)} />
+          <div className="col-3">
+            <TextField id="inputPassword2"
+              maxLength={12}
+              className="form-control"
+              type='password'
+              label="Confirme a Senha"
+              variant="outlined"
+              value={password2}
+              onChange={(e) => setPassword2(e.target.value)} />
 
           </div>
         </div>
